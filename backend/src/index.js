7aec7b6876d5f17197cc38d05e9a19c9a1dbcae8
Vaibhav Mittal <vaibhavmittal146@gmail.com>
2025-03-16 19:54:59 +0000

    added auth.route.js file for basic login and logout

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import authRoutes from './routes/auth.route.js';  
+
 const app = express();
 
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);  
+
 app.listen(5001, () => {
-    console.log('Server is running at port 5001')
-})
+    console.log('Server is running at port 5001');
+});
