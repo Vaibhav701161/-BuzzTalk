1db019cbbc8138c59afb1ae0a2b706ef9c4b2a25
Vaibhav Mittal <vaibhavmittal146@gmail.com>
2025-03-16 20:20:33 +0000

    added auth.controller.js to handle request processing and set up mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';  
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT
 
 app.use(express.json());  
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);  
 
-app.listen(5001, () => {
-    console.log('Server is running at port 5001');
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
 });
