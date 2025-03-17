921603ed5840cccfbbfec333ca43e8cb9b090f39
Vaibhav701161 <vaibhavmittal929@gmail.com>
2025-03-17 08:34:57 +0000

    added encryption and error handling for login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -43,8 +43,31 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login =(req, res) => {
-    res.send("Login route");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await
+        User.findOne({email});
+        if (!user) {
+            return res.status(400).json({message: "User not found"});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        generateToken(user._id,res)
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.fullName,
+            email:user.email,
+            profilePicture: user.profilePicture,
+        });
+    } catch (error) {
+        console.log("error in login controller", error.message);
+        res.status(200).json({message:"internal server error"});
+    }
 };
 
 export const logout =(req, res) => {
